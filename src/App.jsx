5d85fb9413165938cde6420d2d261aeea9d7d65e
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2024-04-12 14:32:22 +0200

    Add new fonts and update styles in index.html and styles.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from './components/layout/Layout';
 import Home from './components/Home';
 
@@ -7,11 +8,13 @@ import Home from './components/Home';
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} index />
-        </Routes>
-      </Layout>
+      <div className="background">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} index />
+          </Routes>
+        </Layout>
+      </div>
     </BrowserRouter>
   );
 }
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from './Footer';
 
 function Layout({ children }) {
     return (
-        <div>
+        <div className='site'>
             <Header />
             <main>{children}</main>
             <Footer />
