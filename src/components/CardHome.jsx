d49c8b93cf652a1d76310e369a60ce4a4166dd9e
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2024-04-29 15:07:38 +0200

    Update styles.css with new button styles, card styles, and layout changes

---
+++ src/components/CardHome.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import ExampleImage from "../img/clement-proust-bDGgOlguqJA-unsplash.jpg";
+
+function CardHome() {
+    return (<Card className='cardBorder flex-grow-1 d-flex flex-column overflow--hidden'>
+        <div className='imgContainer'>
+            <Card.Img variant="top" src={ExampleImage} className='imgCardBorder imgResponsive' />
+        </div>
+        <Card.Body className='bodyCardBorder'>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+            </Card.Text>
+        </Card.Body>
+        <Card.Footer className="footerCardBorder d-flex justify-content-between align-items-center">
+            <span>Price</span>
+            <Link to="/cardPage"><Button variant="outline-success">More</Button></Link>
+        </Card.Footer>
+    </Card>);
+}
+
+export default CardHome;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,54 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
+import { Container } from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
+import CardHome from './CardHome';
 
 function Home() {
     return (
-        <Row xs={1} md={2} lg={4} className="g-4 my-5 mx-2">
-            {Array.from({ length: 12 }).map((_, idx) => (
-                <Col key={idx}>
-                    <Card className='cardBorder'>
-                        <Card.Img variant="top" src="holder.js/100px160" className='imgCardBorder' />
-                        <Card.Body className='bodyCardBorder'>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This is a longer card with supporting text below as a natural
-                                lead-in to additional content. This content is a little bit
-                                longer.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer className="footerCardBorder d-flex justify-content-between align-items-center">
-                            <span>Price</span>
-                            <Link to="/cardPage"><Button variant="outline-success">More</Button></Link>
-                        </Card.Footer>
-                    </Card>
+        <Container fluid className='cardHomeWidth'>
+            <Row className='h-100'>
+                <Col lg={8} className="d-flex flex-column flex-grow-1"><CardHome className="flex-grow-1" /></Col>
+                <Col lg={4} className="d-flex flex-column flex-grow-1">
+                    <Row className='h-100'><CardHome className="flex-grow-1 imgCustomSize" /></Row>
+                    <Row className='h-100'><CardHome className="flex-grow-1 imgCustomSize" /></Row>
                 </Col>
-            ))}
-        </Row>
+            </Row>
+            <Row>
+                <Col lg={4} className="d-flex flex-column flex-grow-1">
+                    <Row className='h-100'><CardHome className="flex-grow-1 imgCustomSize" /></Row>
+                    <Row className='h-100'><CardHome className="flex-grow-1 imgCustomSize" /></Row>
+                </Col>
+                <Col lg={8} className="d-flex flex-column flex-grow-1"><CardHome className="flex-grow-1" /></Col>
+            </Row>
+        </Container>
     );
+
+    //     <Row xs={1} md={2} lg={4} className="g-4 my-5 mx-2">
+    //         {Array.from({ length: 12 }).map((_, idx) => (
+    //             <Col key={idx}>
+    //                 <Card className='cardBorder'>
+    //                     <Card.Img variant="top" src="holder.js/100px160" className='imgCardBorder' />
+    //                     <Card.Body className='bodyCardBorder'>
+    //                         <Card.Title>Card title</Card.Title>
+    //                         <Card.Text>
+    //                             This is a longer card with supporting text below as a natural
+    //                             lead-in to additional content. This content is a little bit
+    //                             longer.
+    //                         </Card.Text>
+    //                     </Card.Body>
+    //                     <Card.Footer className="footerCardBorder d-flex justify-content-between align-items-center">
+    //                         <span>Price</span>
+    //                         <Link to="/cardPage"><Button variant="outline-success">More</Button></Link>
+    //                     </Card.Footer>
+    //                 </Card>
+    //             </Col>
+    //         ))}
+    //     </Row>
+    // );
 }
 
 export default Home;
