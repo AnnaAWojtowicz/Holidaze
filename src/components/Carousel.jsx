d7c5026c06ff11e97c828b25413ee52ef853a784
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2024-04-29 15:55:01 +0200

    Update styles.css with new button styles, card styles, and Carousel component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -12,28 +12,14 @@ function ControlledCarousel() {
 
     return (
         <Carousel activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item>
-                <img src={ExampleImage} />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
+            <Carousel.Item className="imgContainer2">
+                <img src={ExampleImage} className="imgCardBorder imgResponsive" />
             </Carousel.Item>
-            <Carousel.Item>
-                <img src={ExampleImage} />
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
+            <Carousel.Item className="imgContainer2">
+                <img src={ExampleImage} className="imgCardBorder imgResponsive" />
             </Carousel.Item>
-            <Carousel.Item>
-                <img src={ExampleImage} />
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                </Carousel.Caption>
+            <Carousel.Item className="imgContainer2">
+                <img src={ExampleImage} className="imgCardBorder imgResponsive" />
             </Carousel.Item>
         </Carousel>
     );
