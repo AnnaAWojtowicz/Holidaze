1c75986cc97005b1c701caf9d8f81987ed67b0de
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2024-05-18 00:17:41 +0200

    refactor: Update BookingCard component to improve UI layout and add Go Back button

--- src/components/profile/BookingCard.jsx
+++ src/components/profile/BookingCard.jsx
@@ -5,6 +5,7 @@ import { ListGroup } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getBookingDetails } from '../../api/bookingDetails';
+import { Link } from 'react-router-dom';
 
 function BookingCard() {
     const { id } = useParams();
@@ -29,18 +30,24 @@ function BookingCard() {
     }
 
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Header>{booking.venue.name}</Card.Header>
-            <ListGroup variant="flush">
-                <ListGroup.Item>Name: {customerName}</ListGroup.Item>
-                <ListGroup.Item>Email: {booking.customer.email}</ListGroup.Item>
-                <ListGroup.Item>
-                    <div>Date from: {new Date(booking.dateFrom).toLocaleDateString()}</div>
-                    <div>Date to: {new Date(booking.dateTo).toLocaleDateString()}</div>
-                </ListGroup.Item>
-                <ListGroup.Item>Number of guests: {booking.guests}</ListGroup.Item>
-            </ListGroup>
-        </Card>
+        <div className="my-5 cardPage">
+            <Card className="mx-4 cardBorder cardWidth">
+                <Card.Header className='bodyCardBorder'>
+                    <Card.Title className="nameCardProfile">{booking.venue.name}</Card.Title></Card.Header>
+                <ListGroup variant="flush" className='bodyCardBorder'>
+                    <ListGroup.Item>Name: {customerName}</ListGroup.Item>
+                    <ListGroup.Item>Email: {booking.customer.email}</ListGroup.Item>
+                    <ListGroup.Item>
+                        <div>Date from: {new Date(booking.dateFrom).toLocaleDateString()}</div>
+                        <div>Date to: {new Date(booking.dateTo).toLocaleDateString()}</div>
+                    </ListGroup.Item>
+                    <ListGroup.Item>Number of guests: {booking.guests}</ListGroup.Item>
+                </ListGroup>
+                <Card.Footer className="footerCardBorder d-flex justify-content-between align-items-center">
+                    <Link to="/"><Button variant="outline-success">Go Back</Button></Link>
+                </Card.Footer>
+            </Card>
+        </div>
     );
 }
 
